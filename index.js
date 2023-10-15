d1b5204d1ebbca7ef8bcb0ac820d6e4761f3d6af
bill <bill.arnold@student.moringaschool.com>
2023-10-15 20:49:32 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    // This function will be called when the DOM is fully loaded and ready for manipulation
+    const paragraphElement = document.getElementById('text');
+  
+    // Check if the paragraph element exists
+    if (paragraphElement) {
+      // Replace the text content of the paragraph
+      paragraphElement.textContent = "This is really cool!";
+    } else {
+      console.error("Paragraph element with id 'text' not found");
+    }
+  });
+  
